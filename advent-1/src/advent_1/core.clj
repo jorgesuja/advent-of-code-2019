23df0a3115fee91140a0b0f87e6b283a1dbcdf5f
Jorge Suja <jorgesuja@hotmail.com>
2019-12-02 23:08:55 +0100

    First kata

---
+++ advent-1/src/advent_1/core.clj
@@ -0,0 +1,36 @@
+(ns advent-1.core
+  (:require [clojure.string :as str]
+            [clojure.java.io :as io])
+  (:gen-class))
+
+(defn- compute-fuel
+  "Computes the fuel needed to launch a module"
+  [mass]
+  (- (Math/floor (/ mass 3)) 2))
+
+(defn- parse-file
+  "Reads the input file into a vector"
+  [file]
+  (with-open [rdr (io/reader file)]
+    (->> (line-seq rdr)
+         (into []))))
+
+(defn- process-values
+  "Processes all the read values"
+  [values]
+  (->> values
+       (map #(compute-fuel (Integer/parseInt %)))
+       (reduce +)))
+
+(defn- write-results
+  "Write results to the console"
+  [result]
+  (println "Total mass:" result))
+
+(defn -main
+  "Entry point to the app"
+  [& args]
+  (-> (io/resource "modules.txt")
+      parse-file
+      process-values
+      write-results))
